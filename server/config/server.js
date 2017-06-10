f4a8fa4589f44f11bb78210238d4c8fb2b2c75a2
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-06-10 20:11:54 +0100

    feature(configuration): webpack for node server   [Finishes #146976579]

--- server/config/server.js
+++ server/config/server.js
@@ -1,18 +1,26 @@
-const express = require('express');
-
 const logger = require('morgan');
+const path = require('path');
+const webpack = require('webpack');
+const express = require('express');
+const webpackMiddleware = require('webpack-dev-middleware');
+const config = require('../../webpack.config');
+
 
 const bodyParser = require('body-parser');
 
 const app = express();
+const compiler = webpack(config);
 app.use(logger('dev'));
 
 const port = process.env.PORT || 8080;
 const env = process.env.NODE_ENV || 'development';
 
+app.use(express.static(`${__dirname}../../client/app/public`));
+app.use(webpackMiddleware(compiler));
 app.set('jswtSecret', process.env.JWTSECRET);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
@@ -20,8 +28,11 @@ app.listen(port, () => {
 
 require('../routes/route')(app);
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to document management',
-}));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+// app.get('*', (req, res) => res.status(200).send({
+//   message: 'Welcome to document management',
+// }));
 module.exports = app;
 
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('dotenv').config();
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: './app/components/main.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/public`
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+
+    rules: [
+      {
+        test: /\.js|.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader', 'resolve-url-loader', 'sass-loader?sourceMap'],
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader', 'style-loader', 'resolve-url-loader'],
+      },
+
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=1000000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg|png|jpe?g)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader?name=/[name].[ext]'
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+    ]
+  },
+};
+module.exports = config;
