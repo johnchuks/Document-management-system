7583b5c8a08e27e3f47a060e507b1671f44adbbe
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-06-17 20:10:36 +0100

    feature(signup): fix bug in signuppage.jsx file  [Finishes #147044323]

--- client/app/src/actions/createUserActions.js
+++ client/app/src/actions/createUserActions.js
@@ -2,14 +2,18 @@ import axios from 'axios';
 import { createUser } from '../api/Api';
 import { CREATE_USERS, FETCH_USERS } from '../constants/actionTypes';
 
-export const signupAction = (userData) => createUser(userData)
-  .then(result => ({
-    type: CREATE_USERS,
-    payload: result
-  }));
+const signupAction = (userData) => {
+  return createUser(userData)
+    .then(result => ({
+      type: CREATE_USERS,
+      payload: result
+    }));
+};
 
-export const fetchUserAction = () => ({
+const fetchUserAction = () => ({
   type: FETCH_USERS,
   payload: axios.get('/api/users').then(response => response.data)
 });
+
+export { signupAction, fetchUserAction };
 
