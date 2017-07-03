3396ab22a8666e73781386c8feecefe59604a550
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-07-03 17:48:23 +0100

    chore(heroku): fix config.json file  [Finishes #148241615]

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 
-console.log(process.env.DATABASE_URL_PROD);
-
 module.exports = {
-  "development": {
-    "use_env_variable":"DATABASE_URL_PROD",
-    "dialect":"postgres"
+  development: {
+    use_env_variable:"DATABASE_URL_PROD",
+    dialect:"postgres"
   },
-  "test": {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres"
+  test: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
   }
 }
